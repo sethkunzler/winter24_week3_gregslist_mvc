80734eb6427357740e23734937e17ba9b78c10ae
Seth Kunzler <sethkunzler@gmail.com>
2024-02-07 17:25:27 -0700

    Updated the House Model to include an Html Template

--- app/controllers/HousesController.js
+++ app/controllers/HousesController.js
@@ -1,12 +1,25 @@
-// This is my Houses Controller. it will be tracking interactive information on the Houses page and it will also drawing updated information to that page
+// imports
+import { AppState } from "../AppState.js"
+
 // #region this is where my private functions will go. private functions can only be accessed on the HousesController.js
+function _drawHouses() {
+  const houses = AppState.houses
+  let HTMLstring = ``
+  // cars.forEach(car => htmlString += car.CarCardHTMLTemplate)
+  houses.forEach(house => HTMLstring += house.HouseCardHTMLtemplate)
+}
 
 // #endregion 
+
+// This is my Houses Controller. it will be tracking interactive information on the Houses page and it will also drawing updated information to that page
 export class HousesController {
   // Constructors run on page load
   constructor () {
     console.log("Houses Controller loaded")
-    
+
+
+    // My Event listeners 
+    AppState.on("houses", _drawHouses)
   }
 
 }
--- app/models/House.js
+++ app/models/House.js
@@ -18,3 +18,55 @@ export class House {
   }
 
 }
+
+get HouseCardHTMLtemplate() {
+  return /*html*/ `
+        <div class="col-12 col-md-8">
+          <div class="card">
+          <img 
+          class="house-img rounded-start"
+          src="${this.imgUrl}" 
+          alt="Picture of a ${this.bedrooms} bed / ${this.bathrooms} bath house in ${this.city} for $${this.price}">
+          <div>
+            <h2>${this.bedrooms} bed / ${this.bathrooms} bath house in ${this.city}</h2>
+            <h2>$${this.price}</h2>
+            <h3>Built in: ${this.year}</h3>
+            <h4>Total Square Footage: ${this.sqft}ft<sup>2</sup></h4>
+            <h4>Description: </h4>
+            <h5>${this.description}</h5>
+          </div>
+          </div>
+        </div>
+  `
+}
+
+// get CarCardHTMLTemplate() {
+//   return `
+//   <div class="col-12 mb-3">
+//     <div class="bg-light rounded shadow-lg d-block d-md-flex">
+//       <img class="car-img rounded-start"
+//         src="${this.imgUrl}"
+//         alt="Picture of ${this.make} ${this.model}">
+
+//       <div class="py-2 px-5">
+//         <h2>${this.year} ${this.make} ${this.model}</h2>
+//         <h2>$${this.price}</h2>
+//         <h3>${this.mileage} Miles</h3>
+//         <h4>Listed on ${this.ListedAtDate}</h4>
+//         <h4>At ${this.ListedAtTime}</h4>
+//         <p>${this.description}</p>
+//         <div class="d-flex">
+//           <p>Car Color:</p>
+//           <div class="ms-4 car-color-circle" style="background-color: ${this.color};"></div>
+//         </div>
+//         <div class="fs-1">
+//           ${this.SalvagedTitleIcon}
+//         </div>
+//         <div>
+//           <button onclick="app.CarsController.removeCar('${this.id}')" class="btn btn-danger">Delete Car</button>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+//   `
+// }
